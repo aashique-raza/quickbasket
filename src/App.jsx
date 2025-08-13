d9e2b97a8b9d8b770381fa213ea5bb2e7aa7ea2a
farhan ansari <farhanraza2239@gmail.com>
2025-08-13 19:23:44 +0530

    Initial commit: setup React Vite + Tailwind + Redux

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className=' text-red-700'>hello quick basket</h1>
+    </>
+  )
+}
+
+export default App
